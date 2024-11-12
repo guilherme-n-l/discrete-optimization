9e9fee2626dd9450987818c2466b905d86b564c5
guilherme-n-l <acc.guilhermenl@gmail.com>
2024-11-12 11:21:47 -0300

    (Coloring) Dsatur score 22/60

--- coloring/main.c
+++ coloring/main.c
@@ -4,6 +4,7 @@
 
 typedef struct {
     unsigned int nNeighs;
+    unsigned int satDegree;
     unsigned int color;
     unsigned int* neighs;
 } node_t;
@@ -15,6 +16,8 @@ char getNode(unsigned int idx, node_t **pNode, GHashTable* ht) {
     if (!exists) {
         node_t *node = g_new(node_t, 1);
         node->nNeighs = 0;
+        node->color = -1;
+        node->satDegree = 0;
 
         g_hash_table_insert(ht, GUINT_TO_POINTER(idx), node);
 
@@ -38,30 +41,36 @@ void collectNeighs(unsigned int n, unsigned int e, unsigned int (*st)[2], GHashT
     unsigned int *idxNeighs = calloc(n, sizeof(unsigned int));
 
     for (int i = 0; i < e; i++) {
-        unsigned int *pair = st[i];
-
         node_t *node;
-        unsigned int nodeIdx = pair[0];
+        unsigned int nodeIdx = st[i][0];
         getNode(nodeIdx, &node, ht);
 
-        node->neighs[idxNeighs[nodeIdx]++] = pair[1];
+        node->neighs[idxNeighs[nodeIdx]++] = st[i][1];
 
-        nodeIdx = pair[1];
+        nodeIdx = st[i][1];
         getNode(nodeIdx, &node, ht);
-        node->neighs[idxNeighs[nodeIdx]++] = pair[0];
+        node->neighs[idxNeighs[nodeIdx]++] = st[i][0];
     }
 
     free(idxNeighs);
 }
 
-void testPrint(gpointer k, gpointer v, gpointer args) {
-    node_t *node = (node_t *)v;
-    unsigned int nNeighs = node->nNeighs;
-    printf("%u - %u( ", GPOINTER_TO_UINT(k), nNeighs);
-    for (int i = 0; i < nNeighs; i++) {
-        printf("%u ", node->neighs[i]);
+void printPNode(gpointer k, gpointer v, gpointer args) {
+    node_t *pNode = (node_t *)v;
+
+    printf("Node %u (color=%d) { ", GPOINTER_TO_UINT(k), pNode->color);
+    for (int i = 0; i < pNode->nNeighs; i++) {
+        printf("%u ", pNode->neighs[i]);
     }
-    printf(")\n");
+    printf("} (len=%u)\n", pNode->nNeighs);
+}
+
+void printNode(node_t* pNode) {
+    printf("Node (color=%d) { ", pNode->color);
+    for (int i = 0; i < pNode->nNeighs; i++) {
+        printf("%u ", pNode->neighs[i]);
+    }
+    printf("} (len=%u)\n", pNode->nNeighs);
 }
 
 void getInput(unsigned int n, unsigned int e, GHashTable* ht) {
@@ -79,22 +88,116 @@ void getInput(unsigned int n, unsigned int e, GHashTable* ht) {
         node->nNeighs++;
     }
 
-    for (int i = 0; i < e; i++) {
-        printf("p1(%u) p2(%u)\n", st[i][0], st[i][1]);
-    }
-
     g_hash_table_foreach(ht, mallocNeighs, NULL);
     collectNeighs(n, e, st, ht);
 
     free(st);
 }
 
+int compareHighestDeg(const void *a, const void *b) {
+    return -(((node_t *)a)->nNeighs - ((node_t *)b)->nNeighs);
+}
+
+int compareHighestSatDeg(const void *a, const void *b) {
+
+    unsigned int satA = ((node_t *)a)->satDegree;
+    unsigned int satB = ((node_t *)b)->satDegree;
+
+    if (satA == satB) {
+        return -(satA - satB);
+    }
+
+    return compareHighestDeg(a, b);
+}
+
+void getNeighborColors(node_t *pNode, GHashTable *ht, unsigned int *colors) {
+    for (int i = 0; i < pNode->nNeighs; i++) {
+        node_t *pNeigh;
+        getNode(pNode->neighs[i], &pNeigh, ht);
+
+        colors[i] = pNeigh->color;
+    }
+}
+
+void assignColor(unsigned int n, node_t *pNode, GHashTable *ht) {
+    unsigned int nNeighs = pNode->nNeighs;
+    unsigned int *neighColors = malloc(nNeighs * sizeof(unsigned int));
+
+    getNeighborColors(pNode, ht, neighColors);
+
+    for (unsigned int i = 0; i < n; i++) {
+        unsigned char nodeWithColor = 0;
+
+        for (unsigned int j = 0; j < nNeighs; j++) {
+            node_t *pNeigh;
+            getNode(pNode->neighs[j], &pNeigh, ht);
+
+            nodeWithColor |= pNeigh->color == i;
+
+            if (nodeWithColor) {
+                break;
+            }
+        }
+
+        if (!nodeWithColor) {
+            pNode->color = i;
+            break;
+        }
+    }
+
+    for (int i = 0; i < nNeighs; i++) {
+        node_t *pNeigh;
+        getNode(i, &pNeigh, ht);
+
+        pNeigh->satDegree++;
+    }
+
+    free(neighColors);
+}
+
+unsigned int getNColors(int nNodes, node_t **nodes) {
+    unsigned int nColors = 0;
+    for (int i = 0; i < nNodes; i++) {
+        nColors = MAX(nColors, nodes[i]->color);
+    }
+    return nColors + 1;
+}
+
 int main() {
     unsigned int n, e;  
     scanf("%u %u", &n, &e);
+
     GHashTable *ht = g_hash_table_new(g_direct_hash, g_direct_equal);
     getInput(n, e, ht);
-    g_hash_table_foreach(ht, testPrint, NULL);
+
+    node_t **nodes = malloc(n * sizeof(node_t *));
+
+    for (int i = 0; i < n; i++) {
+        node_t *pNode;
+        getNode(i, &pNode, ht);
+
+        nodes[i] = pNode;
+    }
+
+    qsort(nodes, n, sizeof(node_t *), compareHighestDeg);
+
+    for (int i = 0; i < n; i++) {
+        node_t *pNode = nodes[i];
+        assignColor(n, pNode, ht);
+
+        if (i < n - 1) {
+            qsort(nodes + i + 1, n - (i + 1), sizeof(node_t *), compareHighestSatDeg);
+        }
+    }
+
+    printf("%u 0\n", getNColors(n, nodes));
+    for (int i = 0; i < n; i++) {
+        node_t *pNode;
+        getNode(i, &pNode, ht);
+        printf(i < n - 1 ? "%u " : "%u\n", pNode->color);
+    }
+
+    free(nodes);
     g_hash_table_foreach(ht, freeNeighs, NULL);
     return 0;
 }
--- coloring/solver.py
+++ coloring/solver.py
@@ -1,43 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from subprocess import run
+
 
 def solve_it(input_data):
-    # Modify this code to run your optimization algorithm
-
-    # parse the input
-    lines = input_data.split('\n')
-
-    first_line = lines[0].split()
-    node_count = int(first_line[0])
-    edge_count = int(first_line[1])
-
-    edges = []
-    for i in range(1, edge_count + 1):
-        line = lines[i]
-        parts = line.split()
-        edges.append((int(parts[0]), int(parts[1])))
-
-    # build a trivial solution
-    # every node has its own color
-    solution = range(0, node_count)
-
-    # prepare the solution in the specified output format
-    output_data = str(node_count) + ' ' + str(0) + '\n'
-    output_data += ' '.join(map(str, solution))
-
-    return output_data
+    p = run("./main", input=input_data, capture_output=True, text=True)
+    return p.stdout
 
 
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
-        with open(file_location, 'r') as input_data_file:
+        with open(file_location, "r") as input_data_file:
             input_data = input_data_file.read()
-        print(solve_it(input_data))
+        print(solve_it(input_data), end='')
     else:
-        print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/gc_4_1)')
-
+        print(
+            "This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/gc_4_1)"
+        )
