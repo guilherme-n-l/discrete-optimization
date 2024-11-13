3ac597b26f5f1023892c18dd7bbc311404ca8cc6
guilherme-n-l <acc.guilhermenl@gmail.com>
2024-11-13 17:42:13 -0300

    (Coloring) Optimizing (UNTESTED)

--- coloring/main.c
+++ coloring/main.c
@@ -3,43 +3,47 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define POPULATION_SIZE 5
-#define N_GENERATIONS 5
+#define POPULATION_SIZE 10
+#define N_GENERATIONS 10
 #define MUTATION_RATE 0.1
 
 typedef struct {
+    int id;
     int nNeighs;
-    // int satDegree;
     int color;
     int* neighs;
 } node_t;
 
-char getNode(int idx, node_t **pNode, GHashTable* ht) {
-    *pNode = (node_t *)g_hash_table_lookup(ht, GUINT_TO_POINTER(idx));
+void getNode(int idx, node_t **ppNode, GHashTable* ht) {
+    *ppNode = (node_t *)g_hash_table_lookup(ht, GUINT_TO_POINTER(idx));
 
-    char exists = *pNode != NULL;
-    if (!exists) {
-        node_t *node = g_new(node_t, 1);
-        node->nNeighs = 0;
-        node->color = -1;
-        // node->satDegree = 0;
+    if (*ppNode == NULL) {
+        *ppNode = g_new0(node_t, 1);
+        (*ppNode)->id = idx;
+        (*ppNode)->color = -1;
 
-        g_hash_table_insert(ht, GUINT_TO_POINTER(idx), node);
-
-        *pNode = node;
+        g_hash_table_insert(ht, GUINT_TO_POINTER(idx), *ppNode);
     };
-
-    return exists;
 }
 
 void mallocNeighs(gpointer k, gpointer v, gpointer args) {
-    node_t *node = (node_t *)v;
-    node->neighs = malloc(node->nNeighs * sizeof(int));
+    node_t *pNode= (node_t *)v;
+
+    int nNeighs = pNode->nNeighs;
+
+    if (nNeighs > 0) {
+        pNode->neighs = malloc(nNeighs * sizeof(int));
+        return;
+    }
+
+    pNode->neighs = NULL;
 }
 
 void freeNeighs(gpointer k, gpointer v, gpointer args) {
-    node_t *node = (node_t *)v;
-    free(node->neighs);
+    int *neighs = ((node_t *)v)->neighs;
+    if (neighs != NULL) {
+        free(neighs);
+    }
 }
 
 void freeNodes(gpointer k, gpointer v, gpointer args) {
@@ -50,15 +54,13 @@ void collectNeighs(int n, int e, int (*st)[2], GHashTable* ht) {
     int *idxNeighs = calloc(n, sizeof(int));
 
     for (int i = 0; i < e; i++) {
-        node_t *node;
-        int nodeIdx = st[i][0];
-        getNode(nodeIdx, &node, ht);
+        node_t *pNode;
+        for (int j = 0; j < 2; j++) {
+            int nodeIdx = st[i][j];
 
-        node->neighs[idxNeighs[nodeIdx]++] = st[i][1];
-
-        nodeIdx = st[i][1];
-        getNode(nodeIdx, &node, ht);
-        node->neighs[idxNeighs[nodeIdx]++] = st[i][0];
+            getNode(nodeIdx, &pNode, ht);
+            pNode->neighs[idxNeighs[nodeIdx]++] = st[i][!j];
+        }
     }
 
     free(idxNeighs);
@@ -67,34 +69,32 @@ void collectNeighs(int n, int e, int (*st)[2], GHashTable* ht) {
 // void printPNode(gpointer k, gpointer v, gpointer args) {
 //     node_t *pNode = (node_t *)v;
 //
-//     printf("Node %u (color=%d) { ", GPOINTER_TO_UINT(k), pNode->color);
+//     printf("Node %d (color=%d) { ", pNode->id, pNode->color);
 //     for (int i = 0; i < pNode->nNeighs; i++) {
-//         printf("%u ", pNode->neighs[i]);
+//         printf("%d ", pNode->neighs[i]);
 //     }
-//     printf("} (len=%u)\n", pNode->nNeighs);
+//     printf("} (len=%d)\n", pNode->nNeighs);
 // }
-
+//
 // void printNode(node_t* pNode) {
-//     printf("Node (color=%d) { ", pNode->color);
+//     printf("Node %d (color=%d) { ", pNode->id, pNode->color);
 //     for (int i = 0; i < pNode->nNeighs; i++) {
-//         printf("%u ", pNode->neighs[i]);
+//         printf("%d ", pNode->neighs[i]);
 //     }
-//     printf("} (len=%u)\n", pNode->nNeighs);
+//     printf("} (len=%d)\n", pNode->nNeighs);
 // }
 
 void getInput(int n, int e, GHashTable* ht) {
     int (*st)[2] = malloc(e * sizeof(*st));
 
     for (int i = 0; i < e; i++) {
-        scanf("%u %u", &st[i][0], &st[i][1]);
+        scanf("%d %d", &st[i][0], &st[i][1]);
 
         node_t *node;
-
-        getNode(st[i][0], &node, ht);
-        node->nNeighs++;
-
-        getNode(st[i][1], &node, ht);
-        node->nNeighs++;
+        for (int j = 0; j < 2; j++) {
+            getNode(st[i][j], &node, ht);
+            node->nNeighs++;
+        }
     }
 
     g_hash_table_foreach(ht, mallocNeighs, NULL);
@@ -103,22 +103,7 @@ void getInput(int n, int e, GHashTable* ht) {
     free(st);
 }
 
-// int compareHighestDeg(const void *a, const void *b) {
-//     return -(((node_t *)a)->nNeighs - ((node_t *)b)->nNeighs);
-// }
-
-// int compareHighestSatDeg(const void *a, const void *b) {
-//     int satA = ((node_t *)a)->satDegree;
-//     int satB = ((node_t *)b)->satDegree;
-//
-//     if (satA == satB) {
-//         return -(satA - satB);
-//     }
-//
-//     return compareHighestDeg(a, b);
-// }
-
-void getNeighborColors(node_t *pNode, GHashTable *ht, int *colors) {
+void getNeighborsColors(node_t *pNode, GHashTable *ht, int *colors) {
     for (int i = 0; i < pNode->nNeighs; i++) {
         node_t *pNeigh;
         getNode(pNode->neighs[i], &pNeigh, ht);
@@ -127,78 +112,74 @@ void getNeighborColors(node_t *pNode, GHashTable *ht, int *colors) {
     }
 }
 
-void assignColor(int n, node_t *pNode, GHashTable *ht) {
-    int nNeighs = pNode->nNeighs;
-    int *neighColors = malloc(nNeighs * sizeof(int));
-
-    getNeighborColors(pNode, ht, neighColors);
-
+void assignColors(int n, node_t **nodes , GHashTable *ht) {
     for (int i = 0; i < n; i++) {
-        unsigned char nodeWithColor = 0;
+        node_t *pNode = nodes[i];
 
-        for (int j = 0; j < nNeighs; j++) {
-            node_t *pNeigh;
-            getNode(pNode->neighs[j], &pNeigh, ht);
+        int nNeighs = pNode->nNeighs;
+        int *neighColors = malloc(nNeighs * sizeof(int));
 
-            nodeWithColor |= pNeigh->color == i;
+        getNeighborsColors(pNode, ht, neighColors);
+        for (int j = 0; j < n; j++) {
+            char dup = 0;
+            for (int k = 0; k < nNeighs; k++) {
+                dup |= j == neighColors[k];
 
-            if (nodeWithColor) {
+                if (dup) {
+                    break;
+                }
+            }
+
+            if (!dup) {
+                pNode->color = j;
                 break;
             }
         }
 
-        if (!nodeWithColor) {
-            pNode->color = i;
-            break;
-        }
+        free(neighColors);
     }
 
-    for (int i = 0; i < nNeighs; i++) {
-        node_t *pNeigh;
-        getNode(i, &pNeigh, ht);
-
-        // pNeigh->satDegree++;
-    }
-
-    free(neighColors);
 }
 
 int getNColors(int nNodes, node_t **nodes) {
-    int nColors = 0;
+    int nColors = INT_MIN;
     for (int i = 0; i < nNodes; i++) {
         nColors = MAX(nColors, nodes[i]->color);
     }
+
     return nColors + 1;
 }
 
-void getNodeArray(int nNodes, GHashTable *ht, node_t **nodes) {
+void getNodeArray(int nNodes, GHashTable *ht, node_t **nodes, int* order) {
     for (int i = 0; i < nNodes; i++) {
-        node_t *pNode;
-        getNode(i, &pNode, ht);
-
-        nodes[i] = pNode;
+        getNode(order == NULL ? i : order[i], &nodes[i], ht);
     }
 }
 
-int greedy(int nNodes, GHashTable *ht, int *nodeOrder) {
-    node_t **nodes = malloc(nNodes * sizeof(node_t *));
-    getNodeArray(nNodes, ht, nodes);
+void cleanColor(gpointer k, gpointer v, gpointer args) {
+    ((node_t *)v)->color = -1;
+}
 
-    for (int i = 0; i < nNodes; i++) {
-        node_t *pNode = nodes[nodeOrder[i]];
-        assignColor(nNodes, pNode, ht);
+int greedy(int nNodes, GHashTable *ht, int *nodeOrder, int* colors) {
+    node_t **nodes = malloc(nNodes * sizeof(node_t *));
+    getNodeArray(nNodes, ht, nodes, nodeOrder);
+
+    assignColors(nNodes, nodes, ht);
+
+    if (colors != NULL) {
+        for (int i = 0; i < nNodes; i++) {
+            node_t *pNode;
+            getNode(i, &pNode, ht);
+            colors[i] = pNode->color;
+        }
     }
 
     int nColors = getNColors(nNodes, nodes);
 
     free(nodes);
 
+    g_hash_table_foreach(ht, cleanColor, NULL);
     return nColors;
-}
-
-void cleanColor(gpointer k, gpointer v, gpointer args) {
-    node_t *pNode = (node_t *)v;
-    pNode->color=-1;
 }
 
 void shuffleArray(int n, int *arr) {
@@ -231,20 +212,29 @@ void getPopulation(int nNodes, int *arr[POPULATION_SIZE]) {
 
 void partitionSelection(int *population[POPULATION_SIZE], int scores[POPULATION_SIZE], int selected[2]) {
     int collector = 0;
-
     for (int i = 0; i < POPULATION_SIZE; i++) {
         collector += scores[i];
     }
 
+    int inverseScore[POPULATION_SIZE];
+    for (int i = 0; i < POPULATION_SIZE; i++) {
+        inverseScore[i] = collector - scores[i];
+    }
+
+    collector = 0;
+    for (int i = 0; i < POPULATION_SIZE; i++) {
+        collector += inverseScore[i];
+    }
+
     for (int i = 0; i < 2; i++) {
-        int drained = rand() % collector + 1;
+        int drained = rand() % (collector + 1);
 
         int idx = 0;
         while (drained > 0) {
-            drained -= scores[idx++];
+            drained -= inverseScore[idx++];
         }
 
-        selected[i] = idx;
+        selected[i] = --idx;
     }
 }
 
@@ -259,12 +249,47 @@ void crossover(int nNodes, int* p1, int* p2, int *offspring[2]) {
 
     for (int i = 0; i < 2; i++) {
         offspring[i] = malloc(nNodes * sizeof(int));
-        memcpy(offspring[i], p1, nNodes * sizeof(int));
-        memcpy(offspring[i] + splitIdx, p2 + splitIdx, (nNodes - splitIdx) * sizeof(int));
+        char firstSlice = rand() % 2;
+        
+        memcpy(offspring[i], firstSlice ? p1 : p2, nNodes * sizeof(int));
+        memcpy(offspring[i] + splitIdx, (firstSlice ? p2 : p1) + splitIdx, (nNodes - splitIdx) * sizeof(int));
 
         if ((float)rand() / RAND_MAX < MUTATION_RATE) {
             swapPositions(rand() % nNodes, rand() % nNodes, offspring[i]);
         }
+
+        int *freq = calloc(nNodes, sizeof(int));
+        int nDuplicates = 0;
+
+        for (int j = 0; j < nNodes; j++) {
+            freq[offspring[i][j]]++;
+            if (freq[offspring[i][j]] > 1) {
+                nDuplicates++;
+            }
+        }
+
+        while (nDuplicates > 0) {
+            int aV = -1, bV = -1, dupIdx = -1;
+
+            for (int j = 0; j < nNodes; j++) {
+                if (freq[j] == 0) {
+                    aV = j;
+                } else if (freq[j] > 1) {
+                    bV = j;
+                }
+            }
+
+            for (int j = 0; j < nNodes; j++) {
+                if (offspring[i][j] == bV) {
+                    dupIdx = j;
+                }
+            }
+
+            offspring[i][dupIdx] = aV;
+            nDuplicates--;
+        }
+
+        free(freq);
     }
 }
 
@@ -273,23 +298,40 @@ void weakerSelection(int selected[2], int losers[2]) {
     int *range = malloc(rangeLen * sizeof(int));
 
     int i = 0, j = 0;
-    while (i < POPULATION_SIZE) {
+    while (j < rangeLen) {
         if (i != selected[0] && i != selected[1]) {
             range[j++] = i;
         }
         i++;
     }
 
-    for (int i = 0; i < 2; i++) {
-        losers[i] = range[rand() % rangeLen];
-    }
+    losers[0] = range[rand() % rangeLen];
+
+    swapPositions(losers[0], rangeLen - 1, range);
+
+    losers[1] = range[rand() % (rangeLen - 1)];
 
     free(range);
 }
 
+int arrayArgMin(int n, int array[]) {
+    int min = INT_MAX;
+    int idx = 0;
+
+    for (int i = 0; i < n; i++) {
+        min = MIN(min, array[i]);
+
+        if (min == array[i]) {
+            idx = i;
+        }
+    }
+
+    return idx;
+}
+
 int main() {
     int n, e;  
-    scanf("%u %u", &n, &e);
+    scanf("%d %d", &n, &e);
 
     GHashTable *ht = g_hash_table_new(g_direct_hash, g_direct_equal);
     getInput(n, e, ht);
@@ -297,12 +339,12 @@ int main() {
     int *population[POPULATION_SIZE];
     getPopulation(n, population);
 
-    for (int i = 0; i < N_GENERATIONS; i++) {
-        int scores[POPULATION_SIZE];
+    int *bestInput;
 
-        for (int j = 0; j < POPULATION_SIZE; j++) {
-            scores[j] = greedy(n, ht, population[j]);
-            g_hash_table_foreach(ht, cleanColor, NULL);
+    for (int _ = 0; _ < N_GENERATIONS; _++) {
+        int scores[POPULATION_SIZE];
+        for (int i = 0; i < POPULATION_SIZE; i++) {
+            scores[i] = greedy(n, ht, population[i], NULL);
         }
 
         int selected[2];
@@ -313,22 +355,27 @@ int main() {
 
         int *offspring[2];
         crossover(n, population[selected[0]], population[selected[1]], offspring);
+
         for (int i = 0; i < 2; i++) {
             free(population[losers[i]]);
             population[losers[i]] = offspring[i];
         }
+
+        bestInput = population[arrayArgMin(POPULATION_SIZE, scores)];
     }
 
+    int *colors = malloc(n * sizeof(int));
+    printf("%d 0\n", greedy(n, ht, bestInput, colors));
 
     for (int i = 0; i < n; i++) {
-        node_t *pNode;
-        getNode(i, &pNode, ht);
-        printf(i < n - 1 ? "%u " : "%u\n", pNode->color);
+        printf(i < n - 1 ? "%d " : "%d\n", colors[i]);
     }
 
     for (int i = 0; i < POPULATION_SIZE; i++) {
         free(population[i]);
     }
+
+    free(colors);
     g_hash_table_foreach(ht, freeNeighs, NULL);
     g_hash_table_foreach(ht, freeNodes, NULL);
     g_hash_table_destroy(ht);
