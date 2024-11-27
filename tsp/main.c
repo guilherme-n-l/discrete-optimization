da3184bbcf8ab2633ea5e99936315942aee2462a
guilherme-n-l <acc.guilhermenl@gmail.com>
2024-11-27 20:46:14 -0300

    (TSP) Proper init

--- tsp/main.c
+++ tsp/main.c
@@ -2,85 +2,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define square(A) pow(A, 2)
-#define distance(A, B) sqrt(square(A[0] - B[0]) + square(A[1] - B[1]))
+#define SQUARE(A) pow(A, 2)
+#define MAX(A, B) ((A) > (B) ? (A) : (B))
+#define MIN(A, B) ((A) < (B) ? (A) : (B))
+#define CALC_DISTANCE(A, B) sqrt(SQUARE(points[A][0] - points[B][0]) + SQUARE(points[A][1] - points[B][1]))
+#define DISTANCE_IDX(A, B) ((MAX(A, B) - 1) * MAX(A, B) / 2 + MIN(A, B))
+#define DISTANCE(A, B) A == B ? .0 : (distances[DISTANCE_IDX(A, B)] != .0 ? distances[DISTANCE_IDX(A, B)] : (distances[DISTANCE_IDX(A, B)] = CALC_DISTANCE(A, B)))
+#define DISTANCES_LEN (n * (n - 1) / 2)
 
-typedef struct {
-    double  coords[2];
-    int     distances_len;
-    double* distances;
-} point_t;
+typedef double point_t[2];
 
-void get_coords(int n, point_t *points) {
-    for (int i = 0; i < n; i++) {
-        points[i] = (point_t){};
-        (void)scanf("%lf %lf", &points[i].coords[0], &points[i].coords[1]);
-    }
-}
+int n;
+point_t *points;
+double *distances;
 
-void get_distances(int n, point_t *points) {
-    for (int i = 0; i < n; i++) {
-        point_t *p_ptr = &points[i];
-
-        p_ptr->distances_len = n;
-        p_ptr->distances = malloc(p_ptr->distances_len * sizeof(double));
-
-        for (int j = 0; j < p_ptr->distances_len; j++) {
-            p_ptr->distances[j] = -1;
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        point_t *p_ptr = &points[i];
-
-        for (int j = 0; j < p_ptr->distances_len; j++) {
-            point_t *p1_ptr = &points[j];
-
-            if (j == i) {
-                p_ptr->distances[j] = 0;
-            }
-
-            double neigh_distance = p1_ptr->distances[i];
-            p_ptr->distances[j] = neigh_distance != -1 ? neigh_distance : distance(p_ptr->coords, p1_ptr->coords);
-        }
-    }
-}
-
-void get_points(int n, point_t *points) {
-    get_coords(n, points);
-    get_distances(n, points);
-}
-
-void print_point(point_t p) {
-    printf("Point: coords (%.4lf, %.4lf), distances [ ", p.coords[0], p.coords[1]);
-
-    for (int i = 0; i < p.distances_len; i++) {
-        printf("%.4lf ", p.distances[i]);
-    }
-
+void print_double_array(size_t n, double *arr) {
+    printf("[ ");
+    for (size_t i = 0; i < n; i++) printf("%lf ", arr[i]);
     printf("]\n");
 }
 
-void free_points(int n, point_t* points) {
+void get_points() {
     for (int i = 0; i < n; i++) {
-        free(points[i].distances);
+        (void)scanf("%lf %lf", &points[i][0], &points[i][1]);
     }
+}
 
-    free(points);
+void print_point(int idx) {
+    printf("Point: coords (%.4lf, %.4lf) distances [ ", points[idx][0], points[idx][1]);
+    for (int i = 0; i < n; i++) printf("%lf ", DISTANCE(idx, i));
+    printf("]\n");
 }
 
 int main() {
-    int n;
     (void)scanf("%d", &n);
 
-    point_t *points = malloc(n * sizeof(point_t));
-    get_points(n, points);
+    distances = malloc(DISTANCES_LEN * sizeof(double));
+    points = malloc(n * sizeof(point_t));
+    get_points();
 
-    for (int i = 0; i < n; i++) {
-        print_point(points[i]);
-        printf("\n");
-    }
+    for (int i = 0; i < n; i++) print_point(i);
     
-    free_points(n, points);
+    free(distances);
+    free(points);
     return 0;
 }
